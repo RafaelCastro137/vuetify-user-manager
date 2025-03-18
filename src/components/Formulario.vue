d9d4ad07587ac62d5bd4c35303839c4787a5063b
C1322418 Rafael Castro de Oliveira <c1322418@interno.bb.com.br>
2025-03-18 18:14:11 -0300

    Funcionalidade Editar e Modal

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,9 @@
 <template>
+
+
   <v-container class="d-flex flex-column align-center justify-center min-vh-100">
+    <v-alert v-if="alertError" closable title="Campo não informado" class="mb-5" width="500"
+      text="Verifique se os campos foram preenchidos corretamente" type="error" variant="tonal"></v-alert>
     <v-row class="text-center">
       <v-card class="pa-5" width="500">
         <h1 class="text-overline">Cadastrar usuário</h1>
@@ -7,17 +11,15 @@
           <v-img :src="usuario" alt="Logo" max-width="200" class="mb-5" />
         </v-col>
 
-        <v-text-field v-model="form.name" label="Nome" outlined dense></v-text-field>
-        <v-text-field v-model.number="form.age" label="Idade" type="number" outlined dense></v-text-field>
-        <v-text-field v-model="form.city" label="Cidade" outlined dense></v-text-field>
-
+        <v-text-field v-model="form.name" variant="underlined" label="Nome" outlined dense></v-text-field>
+        <v-text-field v-model.number="form.age" variant="underlined" label="Idade" type="number" outlined dense></v-text-field>
+        <v-text-field v-model="form.city" label="Cidade" variant="underlined" outlined dense></v-text-field>
+        <v-select v-model="form.position" variant="underlined" label="Cargo" :items="['Convidado', 'Admin', 'Gerente']"></v-select>
         <v-btn @click="submitForm" color="primary" block class="mt-4">
           Enviar
         </v-btn>
       </v-card>
     </v-row>
-    <v-alert v-if="alertError" closable title="Campo não informado"
-      text="Verifique se os campos foram preenchidos corretamente" type="warning"></v-alert>
   </v-container>
 
 </template>
@@ -33,7 +35,9 @@ export default {
     const form = ref({
       name: '',
       age: null,
-      city: ''
+      city: '',
+      position: ''
+
     });
 
     const alertError = ref(false);
@@ -42,14 +46,15 @@ export default {
       if (
         form.value.name === '' ||
         form.value.age === null ||
-        form.value.city === ''
+        form.value.city === '' ||
+        form.value.position === '' 
       ) {
         alertError.value = true;
       }
       else {
         alertError.value = false;
         emit('form-enviado', form.value);
-        form.value = { name: '', age: null, city: '' };
+        form.value = { name: '', age: null, city: '', position: '' };
       }
     };
 
--- src/components/Tabela.vue
+++ src/components/Tabela.vue
@@ -1,7 +1,10 @@
 <template>
   <v-container class="d-flex flex-column align-center justify-center min-vh-100">
     <v-card class="pa-5" width="800">
-
+      <v-alert v-if="alert" closable title="Usuários salvo" class="mb-5" width="500"
+      text="As alterações foram salvas com sucesso." type="success" variant="tonal"></v-alert>
+      <v-alert v-if="alertDelete" closable title="Usuário excluído" class="mb-5" width="500"
+      text="Usuário foi excluído com sucesso." type="info" variant="tonal"></v-alert>
       <h1 class="text-overline">Lista de Usuário</h1>
       <v-table density="compact">
         <thead>
@@ -16,6 +19,9 @@
               Cidade
             </th>
             <th class="text-left font-weight-black">
+              Cargo
+            </th>
+            <th class="text-left font-weight-black">
             </th>
           </tr>
         </thead>
@@ -24,7 +30,12 @@
             <td>{{ item.name }}</td>
             <td>{{ item.age }}</td>
             <td>{{ item.city }}</td>
+            <td>{{ item.position }}</td>
             <td class="text-right">
+              <v-btn @click="editar(item, index)" class="ma-2" color="green">
+                Editar
+                <v-icon icon="mdi-pencil" end></v-icon>
+              </v-btn>
               <v-btn @click="excluir(index)" class="ma-2" color="red">
                 Excluir
                 <v-icon icon="mdi-delete" end></v-icon>
@@ -36,20 +47,85 @@
       <p v-if="items.length === 0" class="text-center mt-6 pt-5 mb-5"> Não há usuários cadastrados</p>
     </v-card>
   </v-container>
+
+  <div class="text-center pa-4">
+    <v-dialog v-model="dialog" max-width="600">
+      <v-card prepend-icon="mdi-account" title="User Profile">
+        <v-card-text>
+          <v-text-field v-model="usuarioSelecionado.name"  variant="underlined" label="Nome" required></v-text-field>
+          <v-text-field v-model="usuarioSelecionado.age"  variant="underlined" label="Idade"></v-text-field>
+          <v-text-field v-model="usuarioSelecionado.city"  variant="underlined" label="Cidade" required></v-text-field>
+          <v-select  v-model="usuarioSelecionado.position" variant="underlined" label="Cargo" :items="['Convidado', 'Admin', 'Gerente']"></v-select>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" text="Fechar" variant="tonal" @click="dialog = false"></v-btn>
+          <v-btn color="success" text="Salvar" variant="tonal" @click="salvar"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+  <div class="text-center pa-4">
+    <v-dialog v-model="dialogExcluir" max-width="600" persistent>
+      <v-card
+        prepend-icon="mdi-delete"
+        text="Tem certeza que deseja excluir permanentemente o usuário?"
+        title="Excluir usuário"
+      >
+        <template v-slot:actions>
+          <v-spacer></v-spacer>
+
+          <v-btn @click="dialogExcluir = false"> Cancelar </v-btn>
+
+          <v-btn @click="confirmarExclusao" color="red"> Sim, excluir </v-btn>
+        </template>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Tabela',
   data() {
     return {
+      dialog: ref(false),
+      dialogExcluir: ref(false),
       mostrarHome: true,
+      alert: false,
+      alertDelete: false,
+      usuarioSelecionado: ref({ name: "", age: "", city: "", position: "" }),
+      indexSelecionado: null, 
     };
   },
   methods: {
     excluir(index) {
-    this.items.splice(index, 1);
-  }
+      this.indexSelecionado = index; 
+      this.dialogExcluir = true;
+      this.alert = false;
+    },
+    confirmarExclusao() {
+      this.alertDelete = true;
+      this.dialogExcluir = false;
+      this.items.splice(this.indexSelecionado, 1);
+    },
+    editar(item, index) {
+      this.alert = false;
+      this.usuarioSelecionado = { ...item }; 
+      this.indexSelecionado = index; 
+      this.dialog = true;
+    },
+    salvar() {
+      if (this.indexSelecionado !== null) {
+        this.items[this.indexSelecionado] = { ...this.usuarioSelecionado }; 
+        this.indexSelecionado = null;
+      }
+      this.alert = true;
+      this.dialog = false; 
+    }
   },
   props: {
     items: {
